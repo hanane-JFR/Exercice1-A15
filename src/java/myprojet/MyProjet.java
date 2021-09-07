90b5ce80df53f06d2d05423cdf1096f8e58830fa
hjaaf <hjaaf@192.168.0.19>
2021-09-07 19:48:43 -0400

    creation Methode shapeOne&shapeTwo

--- src/java/myprojet/MyProjet.java
+++ src/java/myprojet/MyProjet.java
@@ -12,6 +12,8 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 
 /**
  * REST Web Service
@@ -41,12 +43,55 @@ public class MyProjet {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * PUT method for updating or creating an instance of MyProjet
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(javax.ws.rs.core.MediaType.APPLICATION_XML)
-    public void putXml(String content) {
+      @GET
+    @Path("afficherdetail&RECANGLE&{val1}&{val2}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getXml(@PathParam("val1")int val1,@PathParam("val2")int val2) {
+        //TODO return proper representation object
+//        return "Le resultat est :"+(valeur1+valeur2);
+                return ShapeTwo(val1,val2);
+
+    }
+        private static String ShapeTwo(int val1,int val2){
+        String msg;
+        double Area;
+        
+    if(val1>0&&val2>0){
+        Area=val1*val2;
+         msg=("{\"Status\":\"OK\",\"Timestamp\":12255645,\"Shape\":\"RECTANGLE\"}"+"Width = "+val1+" Width = "+val2+" Area = "+ Area);
+        
+       
+    }else{
+      msg="{\"Status\":\"Error\",\"Timestamp\":12255645,\"Shape\": \"RECANGLE\",\"Message\":\"Not the good Shape\"}";  
+       
+    }
+        
+        return msg;
+    }
+    
+    @GET
+    @Path("afficherdetail&trapzoid&{val1}&{val2}&{val3}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getXml(@PathParam("val1")int val1,@PathParam("val2")int val2,@PathParam("val3")int val3) {
+        //TODO return proper representation object
+//        return "Le resultat est :"+(valeur1+valeur2);
+                return ShapeTree(val1,val2,val3);
+
+    }
+        private static String ShapeTree(int val1,int val2,int val3){
+        String msg;
+        double Area;
+        
+    if(val1>0&&val2>0&&val3>0){
+        Area=((val1+val2)/0.5)*val3;
+         msg=("{\"Status\":\"OK\",\"Timestamp\":12255645,\"Shape\":\"TRAPEZOID\"}"+"Base1 = "+val1+" Base2 = "+val2+" height = "+ val3+" Area = "+ Area);
+        
+       
+    }else{
+      msg="{\"Status\":\"Error\",\"Timestamp\":12255645,\"Shape\":\"TRAPEZOID\",\"Message\":\"Not the good Shape\"}";  
+       
+    }
+        
+        return msg;
     }
 }
